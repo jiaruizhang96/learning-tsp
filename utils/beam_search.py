4113102f653356e89d42ea911aafc8f409ab5b2f
jiaruizhang96 <skylerzhang52@gmail.com>
2023-11-18 22:16:29 -0500

    Update beam_search.py

--- utils/beam_search.py
+++ utils/beam_search.py
@@ -78,7 +78,8 @@ class BatchBeam(NamedTuple):
                 parent=self.parent[key] if self.parent is not None else None,
                 action=self.action[key] if self.action is not None else None
             )
-        return super(BatchBeam, self).__getitem__(key)
+        #return super(BatchBeam, self).__getitem__(key)
+        return BatchBeam.__getitem__(self, key)
 
     # Do not use __len__ since this is used by namedtuple internally and should be number of fields
     # def __len__(self):
